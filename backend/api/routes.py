41ae42ebebef2cd4a324e1cd7bf6ea7396b47848
GreatMeCloud <zhouray28@gmail.com>
2026-08-01 23:07:02 -0400

    add comments

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter
 from services.routing_service import RoutingService
 
-router = APIRouter()
+#Each file has its own router
+router = APIRouter() #Create a router object
+#will add "app.include_router(router)" to main.py 
 
-routing_service = RoutingService()
+routing_service = RoutingService() #Create RoutingService object
 
-@router.get("/route")
+@router.get("/route") #Decorator
+#When someone is accessing to the /route page, the following codes will be excuted
+
+#Define a function to handle the request
 def get_route(
-    start_lat: float,
-    start_lon: float,
-    end_lat: float,
-    end_lon: float,
+    #arguments
+    start_lat: float, #Start latitude
+    start_lon: float, #Start longitude
+    end_lat: float, #End latitude
+    end_lon: float, #End longitude
 ):
     return routing_service.get_route(
     (start_lat, start_lon),
